98aa323f48fcc3d5de6cd2c69c0edccffe5d4b63
Mariano Benedettini <mariano@assemblar.io>
2024-10-31 15:54:48 -0300

    Space things a bit more

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,22 +11,28 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, modDatetime, description } = frontmatter;
 
   return (
-    <li className="my-6">
+    <li className="my-12">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-block text-lg font-medium text-skin-accent decoration-dashed 
+        underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="text-lg font-medium decoration-dashed hover:underline mb-2">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="text-lg font-medium decoration-dashed hover:underline mb-2">
             {title}
           </h3>
         )}
       </a>
-      <Datetime pubDatetime={pubDatetime} modDatetime={modDatetime} showTime={false} />
+      <Datetime
+        pubDatetime={pubDatetime}
+        modDatetime={modDatetime}
+        showTime={false}
+        className="mb-2"
+      />
       <p>{description}</p>
     </li>
   );
